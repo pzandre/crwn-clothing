58c612ad5bfc0245e7bc0fa7b54a9dbd1e32a84c
André Perez <andre.perez@dengun.com>
2022-08-23 18:12:35 -0300

    ADD: User context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 
 import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
